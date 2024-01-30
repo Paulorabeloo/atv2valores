13740bb7ddcc3f970ef4a63358fd98585894bc3d
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2024-01-30 00:07:02 -0300

    atv2valores

---
+++ atv2valores.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int A, B, X;
+    printf("Digite o valor da primeira variável:\n");
+    scanf("%i", &A);
+    printf("Digite o valor da segunda variável:\n");
+    scanf("%i", &B);
+    X = A + B;
+    printf("O valor da variável X é: %i", X);
+return 0;
+}
